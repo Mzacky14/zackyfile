c6140bffcff2238c0292f954d8016db50563989f
Mzacky14 <zackyrafan17@gmail.com>
2025-12-18 01:08:30 +0700

    Add files via upload

---
+++ pengulangan.py
@@ -0,0 +1,20 @@
+# Biodata (perulangan ke 1)
+print("Saya dari sekolah SMA Manggala dengan jurusan IPA perulangan ke 1")
+
+# Menampilkan "Lab ICT" sebanyak 5 kali
+for i in range(5):
+    print("Lab ICT")
+
+# Biodata (perulangan ke 7)
+print("Saya dari jurusan Akuntansi. perulangan ke 7")
+
+# Menampilkan "Lab ICT" sebanyak 6 kali
+for i in range(6):
+    print("Lab ICT")
+
+# Biodata (perulangan ke 14)
+print("Hallo Nama saya Muhammad Zacky Abimanyu, Hobi saya Basket. perulangan ke 14")
+
+# Menampilkan "Lab ICT" sebanyak 17 kali
+for i in range(17):
+    print("Lab ICT")
